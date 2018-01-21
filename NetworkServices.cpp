d4ade67241c86fe4d904dcf63021ae8aec297d54
Misha <mishaovodenko@gmail.com>
2018-01-22 01:32:52 +0200

    added include to NetworkService

--- NetworkServices.cpp
+++ NetworkServices.cpp
@@ -2,15 +2,16 @@
 // Created by Misha on 21/01/2018.
 //
 #include "NetworkServices.h"
-#include <string>
+#include "DecEnc.h"
+
 using namespace std;
-int NetworkServices::sendMessage(SOCKET curSocket, char * message, int messageSize)
-{
+DecEnc DE;
+
+int NetworkServices::sendMessage(SOCKET curSocket, char *message, int messageSize) {
     return send(curSocket, message, messageSize, 0);
 }
 
-int NetworkServices::receiveMessage(SOCKET curSocket, char * buffer, int bufSize)
-{
+int NetworkServices::receiveMessage(SOCKET curSocket, char *buffer, int bufSize) {
     return recv(curSocket, buffer, bufSize, 0);
 }
 
