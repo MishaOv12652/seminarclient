0d6690ecc191f6871878e92f6d6366395f060932
Misha <mishaOvodenko@gmail.com>
2018-01-22 21:28:26 +0200

    added rsa create from char*

--- DecEnc.cpp
+++ DecEnc.cpp
@@ -10,26 +10,45 @@
 #include <openssl/bio.h>
 #include <openssl/err.h>
 #include <stdio.h>
+#include <string>
 
 int padding = RSA_PKCS1_PADDING;
+using namespace std;
 DecEnc::DecEnc() {};
 
-RSA *createRSA(char *filename, int pub) {
-    FILE *fp = fopen(filename, "rb");
-
-    if (fp == NULL) {
-        printf("Unable to open file %s \n", filename);
-        return NULL;
+RSA *createRSA(char * key, int pub) {
+    RSA *rsa = NULL;
+    BIO *keybio;
+    keybio = BIO_new_mem_buf(key, -1);
+    if (keybio == NULL) {
+        printf("Failed to create key BIO");
+        return 0;
     }
-    RSA *rsa = RSA_new();
-
     if (pub) {
-        rsa = PEM_read_RSA_PUBKEY(fp, &rsa, NULL, NULL);
+        rsa = PEM_read_bio_RSA_PUBKEY(keybio, &rsa, NULL, NULL);
     } else {
-        rsa = PEM_read_RSAPrivateKey(fp, &rsa, NULL, NULL);
+        rsa = PEM_read_bio_RSAPrivateKey(keybio, &rsa, NULL, NULL);
+    }
+    if (rsa == NULL) {
+        printf("Failed to create RSA");
     }
 
     return rsa;
+//    FILE *fp = fopen(filename, "rb");
+//
+//    if (fp == NULL) {
+//        printf("Unable to open file %s \n", filename);
+//        return NULL;
+//    }
+//    RSA *rsa = RSA_new();
+//
+//    if (pub) {
+//        rsa = PEM_read_RSA_PUBKEY(fp, &rsa, NULL, NULL);
+//    } else {
+//        rsa = PEM_read_RSAPrivateKey(fp, &rsa, NULL, NULL);
+//    }
+//
+//    return rsa;
 }
 
 int public_encrypt(unsigned char *data, int data_len, char *file_name_key, unsigned char *encrypted) {
