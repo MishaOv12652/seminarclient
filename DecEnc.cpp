7eac8eccb6811619e5c6a83825858d3dffa25278
Misha <mishaovodenko@gmail.com>
2018-01-24 10:42:42 +0200

    DecEnc clean up

--- DecEnc.cpp
+++ DecEnc.cpp
@@ -5,33 +5,13 @@
 #include "DecEnc.h"
 #include <string>
 #include <direct.h>
+
 int padding = RSA_PKCS1_OAEP_PADDING;
 using namespace std;
 
 DecEnc::DecEnc() {};
 
 RSA *DecEnc::createRSA(unsigned char *key, int pub) {
-//    char *workingDir =_getcwd(NULL, 0);
-//    FILE * fp = fopen((char*)key,"r");
-//
-//    if(fp == NULL)
-//    {
-//        perror("Error");
-//        printf("Unable to open file %s \n",key);
-//        return NULL;
-//    }
-//    RSA *rsa= RSA_new() ;
-//
-//    if(pub)
-//    {
-//        rsa = PEM_read_RSA_PUBKEY(fp, &rsa,NULL, NULL);
-//    }
-//    else
-//    {
-//        rsa = PEM_read_RSAPrivateKey(fp, &rsa,NULL, NULL);
-//    }
-//
-//    return rsa;
     RSA *rsa = RSA_new();
     BIO *keybio;
     keybio = BIO_new_mem_buf(key, -1);
@@ -42,14 +22,7 @@ RSA *DecEnc::createRSA(unsigned char *key, int pub) {
     if (pub) {
         rsa = PEM_read_bio_RSA_PUBKEY(keybio, &rsa, NULL, NULL);
     } else {
-//        FILE *fp = fopen((char*)key,"rb");
-//        if(fp==NULL){
-//            perror("Error");
-//            printf("Unable to open file %s \n", key);
-//            return NULL;
-//        }
-            rsa = PEM_read_bio_RSAPrivateKey(keybio, &rsa, NULL, NULL);
-        //rsa = PEM_read_RSAPrivateKey(fp, &rsa, NULL, NULL);
+        rsa = PEM_read_bio_RSAPrivateKey(keybio, &rsa, NULL, NULL);
     }
     if (rsa == NULL) {
         printf("Failed to create RSA");
@@ -72,10 +45,3 @@ int DecEnc::private_decrypt(unsigned char *enc_data, int data_len, unsigned char
 }
 
 
-void printLastError(char *msg) {
-    char *err = new char(130);
-    ERR_load_crypto_strings();
-    ERR_error_string(ERR_get_error(), err);
-    printf("%s ERROR: %s\n", msg, err);
-    free(err);
-}
--- DecEnc.h
+++ DecEnc.h
@@ -24,8 +24,6 @@ public:
     int public_encrypt(unsigned char *data, int data_len, unsigned char *key, unsigned char *encrypted);
 
     int private_decrypt(unsigned char *enc_data, int data_len, unsigned char *key, unsigned char *decrypted);
-
-    void printLastError(char *msg);
 };
 
 
